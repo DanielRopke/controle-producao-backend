27c140f90bcc056e9ed3c852fff94773bc932bfd
Daniel Röpke <daniel.ropke.duarte@gmail.com>
2025-07-03 09:39:49 -0300

    Corrige erro de STATIC_ROOT antes da definição

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -5,9 +5,6 @@ import dj_database_url
 # Caminho base do projeto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# ✅ DEBUG: Verificar se STATIC_ROOT está sendo montado corretamente
-print("STATIC_ROOT DEFINIDO COMO:", STATIC_ROOT)  # Debug
-
 # Chave secreta segura (mantenha segredo em produção!)
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'troque-essa-chave-para-producao')
 
@@ -93,6 +90,9 @@ STATIC_URL = '/static/'
 
 # LOCAL ONDE O COLLECTSTATIC VAI JUNTAR OS ARQUIVOS
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'staticfiles'))
+
+# ✅ DEBUG: Verificar se STATIC_ROOT está sendo montado corretamente
+print("STATIC_ROOT DEFINIDO COMO:", STATIC_ROOT)  # Debug
 
 # DURANTE O DESENVOLVIMENTO (opcional)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
