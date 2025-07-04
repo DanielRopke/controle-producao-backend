c148a6696f33a6c53a412ba485ec23f887cb6b5b
Daniel Röpke <daniel.ropke.duarte@gmail.com>
2025-07-04 00:10:31 -0300

    Mensagem do commit com todas as alterações

--- listar_estrutura.py
+++ listar_estrutura.py
@@ -1,13 +1,13 @@
-# salvar como listar_estrutura.py e rodar com: python listar_estrutura.py
 import os
 
 def listar(diretorio, prefixo=''):
     for nome in sorted(os.listdir(diretorio)):
         caminho = os.path.join(diretorio, nome)
         if os.path.isdir(caminho):
-            print(f"{prefixo} {nome}/")
+            print(f"{prefixo}{nome}/")
             listar(caminho, prefixo + '    ')
         else:
-            print(f"{prefixo} {nome}")
+            print(f"{prefixo}{nome}")
 
-listar('.')
+# Alterar para a pasta que deseja listar, exemplo:
+listar('C:/Users/DANIEL/controle-producao')
